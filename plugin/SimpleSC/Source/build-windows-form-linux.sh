ba344a83e2dce0ef07bf258c09a72353f7e839ac
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-01-28 16:15:59 +0000

    Report progress

--- plugin/SimpleSC/Source/build-windows-form-linux.sh
+++ plugin/SimpleSC/Source/build-windows-form-linux.sh
@@ -1,7 +1,9 @@
 
+echo "This build the NSIS Plugin from sources (cross-compiles it, for win32 target)"
+
 echo -e "\nIn case of problems - read instructions here in depend/ how to prepare dependencies including the cross compiler\n"
 
-fpc -Pi386 -Twin32 -Mdelphi  -Sh   SimpleSC.dpr  
+fpc -Pi386 -Twin32 -Mdelphi  -Sh   SimpleSC.dpr  && echo "OK"
 
 # -S2 is for default value of function argument
 # -Sh http://forum.lazarus.freepascal.org/index.php?topic=6147.0
